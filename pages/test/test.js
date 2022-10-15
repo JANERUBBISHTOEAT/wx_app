fe066f1c95c83834aae2e837dffaf7443863e68b
POD_0x0099 <zheyuan.wei2003@gmail.com>
2022-10-15 00:45:15 -0400

    Commit to preview

--- pages/test/test.js
+++ pages/test/test.js
@@ -22,10 +22,4 @@ Page({
       title: '',
     };
   },
-  bindPickerChange: function(e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    this.setData({
-      index: e.detail.value
-    })
-  },
 });
