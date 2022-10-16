420baea41ed15c5a66f9065cd181ae9a562a39e8
POD_0x0099 <zheyuan.wei2003@gmail.com>
2022-10-16 02:41:01 -0400

    Half way there. Try to get come snacks.

---
+++ pages/tests/test3/questions.js
@@ -0,0 +1,104 @@
+const questions = [
+    {
+        "question": "1. At a party do you: ",
+        "options": {
+            "A": "Interact with many, including strangers",
+            "B": "Interact with a few, known to you",
+        },
+    },
+    {
+        "question": "2. Are you more: ",
+        "options": {
+            "A": "Realistic than speculative",
+            "B": "Speculative than realistic",
+        },
+    },
+    {
+        "question": "3. Is it worse to: ",
+        "options": {
+            "A": "Have your \"head in the clouds\"",
+            "B": "Be \"in a rut\"",
+        },
+    },
+    {
+        "question": "4. Are you more impressed by: ",
+        "options": {
+            "A": "Principles",
+            "B": "Emotions",
+        },
+    },
+    {
+        "question": "5. Are more drawn toward the: ",
+        "options": {
+            "A": "Convincing",
+            "B": "Touching",
+        },
+    },
+    {
+        "question": "6. Do you prefer to work: ",
+        "options": {
+            "A": "To deadlines",
+            "B": "Just \"whenever\"",
+        },
+    },
+    {
+        "question": "7. Do you tend to choose:",
+        "options": {
+            "A": "Rather carefully",
+            "B": "Somewhat impulsively",
+        },
+    },
+    {
+        "question": "8. At parties do you: ",
+        "options": {
+            "A": "Stay late, with increasing energy",
+            "B": "Leave early, with decreasing energy",
+        },
+    },
+    {
+        "question": "9. Are you more attracted to: ",
+        "options": {
+            "A": "Sensible people ",
+            "B": "Imaginative people",
+        },
+    },
+    {
+        "question": "10. Are you more interested in: ",
+        "options": {
+            "A": "What is actual",
+            "B": "What is possible",
+        },
+    },
+    {
+        "question": "11. In judging others are you more swayed by:",
+        "options": {
+            "A": "Laws than circumstances ",
+            "B": "Circumstances than laws",
+        },
+    },
+    {
+        "question": "12. In approaching others is your inclination to be somewhat: ",
+        "options": {
+            "A": "Objective",
+            "B": "Personal",
+        },
+    },
+    {
+        "question": "13. Are you more: ",
+        "options": {
+            "A": "Punctual",
+            "B": "Leisurely",
+        },
+    },
+    {
+        "question": "14. Does it bother you more having things: ",
+        "options": {
+            "A": "Incomplete",
+            "B": "Completed",
+        },
+    }
+]
+
+module.exports = {
+    questions: questions
+}  
--- pages/tests/test3/test3.js
+++ pages/tests/test3/test3.js
@@ -1,45 +1,27 @@
+const questions = require("./questions.js")
 var ans = new Array(20).fill("");
+var qnaire = questions.questions;
 Page({
   data: {
-    checked_1: false,
-    checked_2: false,
-    checked_3: false,
-    checked_4: false,
+    qnaire: qnaire,
+    sliderValue: -1,
+    // checked_1: false,
+    // checked_2: false,
     hide_question: false,
     hide_result: true,
-    index_opt: 0,
-    index_que: 0,
     id: 0, // 1-20
     result: -1,
     result_text: "",
     result_color: "",
-    array_opt: ['A little of time', 'Some of the time', 'Good part of the time', 'Most of the time'],
-    questions: [
-      '1 I feel down-hearted and blue. ',
-      '2 Morning is when I feel the best. ',
-      '3 I have crying spells or feel like it.',
-      '4 I have trouble sleeping at night.',
-      '5 I eat as much as I used to. ',
-      '6 Feeling bad about yourself or that you are a failure or have let yourself or your family down ',
-      '7 I notice that I am losing weight. ',
-      '8 I have trouble with constipation. ',
-      '9 My heart beats faster than usual. ',
-      '10 I get tired for no reason. ',
-      '11 My mind is as clear as it used to be. ',
-      '12 I find it easy to do the things I used to. ',
-      '13 I am restless and can’t keep still. ',
-      '14 I feel hopeful about the future. ',
-      '15 I am more irritable than usual. ',
-      '16 I find it easy to make decisions. ',
-      '17 I feel that I am useful and needed. ',
-      '18 My life is pretty full. ',
-      '19 I feel that others would be better off if I were dead. ',
-      '20 I still enjoy the things I used to do. '
-    ],
     answer: ans,
   },
   onLoad(options) {
-
+    console.log(qnaire[0].question);
+    console.log(qnaire[0].options.A);
+    console.log(qnaire[0].options.B);
+    console.log(qnaire.length);
+    let arr = Array.from(qnaire);
+    console.log(arr[0]);
   },
   onReady() {
 
@@ -68,43 +50,32 @@ Page({
   // radio button will keep your choice when moving to another question.
 
   next_que: function () {
-    var checked_1 = Object.values(ans)[(this.data.id + 1)] == '1';
-    var checked_2 = Object.values(ans)[(this.data.id + 1)] == '2';
-    var checked_3 = Object.values(ans)[(this.data.id + 1)] == '3';
-    var checked_4 = Object.values(ans)[(this.data.id + 1)] == '4';
+    // var checked_1 = Object.values(ans)[(this.data.id + 1)] == '1';
+    // var checked_2 = Object.values(ans)[(this.data.id + 1)] == '2';
     // console.log(this.data.id + ": ", checked_1, checked_2, checked_3, checked_4);
     // console.log(ans);
+
     this.setData({
       id: this.data.id + 1,
-      hide_question: false,
-      checked_1: checked_1,
-      checked_2: checked_2,
-      checked_3: checked_3,
-      checked_4: checked_4,
+      // hide_question: false,
     })
   },
 
-  last_que: function () {
-    // console.log(typeof(this.data.answer), typeof(this.data.id));
-    var checked_1 = Object.values(ans)[(this.data.id - 1)] == '1';
-    var checked_2 = Object.values(ans)[(this.data.id - 1)] == '2';
-    var checked_3 = Object.values(ans)[(this.data.id - 1)] == '3';
-    var checked_4 = Object.values(ans)[(this.data.id - 1)] == '4';
-    // console.log(this.data.id + ": ", checked_1, checked_2, checked_3, checked_4);
+  // last_que: function () {
+  //   // console.log(typeof(this.data.answer), typeof(this.data.id));
+  //   var checked_1 = Object.values(ans)[(this.data.id - 1)] == '1';
+  //   var checked_2 = Object.values(ans)[(this.data.id - 1)] == '2';
+  //   // console.log(this.data.id + ": ", checked_1, checked_2, checked_3, checked_4);
 
-    if (this.data.id != 0) {
-      this.setData({
-        id: this.data.id - 1,
-        hide_question: false,
-        // checked_1: true,
-        checked_1: checked_1,
-        checked_2: checked_2,
-        checked_3: checked_3,
-        checked_4: checked_4,
-        hide_result: true,
-      })
-    };
-  },
+  //   if (this.data.id != 0) {
+  //     this.setData({
+  //       id: this.data.id - 1,
+  //       hide_question: false,
+  //       // checked_1: true,
+  //       hide_result: true,
+  //     })
+  //   };
+  // },
 
   finish_que: function () {
     this.setData({
@@ -113,52 +84,61 @@ Page({
     })
   },
 
+  sliderChange: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      sliderValue: e.detail.value,
+    })
+  },
+
+
   submit: function (e) {
-    // console.log(this.data.id, e.detail.value);
-    ans[this.data.id - 1] = e.detail.value.answer;
+    console.log(this.data.id, this.data.sliderValue);
+    ans[this.data.id - 1] = this.data.sliderValue;
     this.setData({
       answer: ans,
     })
-    // console.log(this.data.answer);
+    console.log(this.data.answer);
   },
 
   sas_analysis: function () {
     var ans = this.data.answer;
-    var total = 0;
+    var E = 0;
+    var I = 0;
+    var S = 0;
+    var N = 0;
+    var T = 0;
+    var F = 0;
+    var J = 0;
+    var P = 0;
     for (var i = 0; i < ans.length; i++) {
 
-      if (i == 2 || i == 5 || i == 11 || i == 12 || i == 14 || i == 16 || i == 17 || i == 18 || i == 20) {
-        total += 5 - parseInt(ans[i]);
+      if (i % 8 == 0) {
+        E += ans[i];
+        I += 5 - ans[i];
       }
-      else {
-        total += parseInt(ans[i]);
+      else if (i % 8 == 1 || i % 8 == 2) {
+        S += ans[i];
+        N += 5 - ans[i];
+      }
+      else if (i % 8 == 3 || i % 8 == 4) {
+        T += ans[i];
+        F += 5 - ans[i];
+      }
+      else if (i % 8 == 5 || i % 8 == 6) {
+        J += ans[i];
+        P += 5 - ans[i];
       }
     }
-    total *= 1.25;
     var result_color = "";
     var result = '';
-    if (total < 50) {
-      result = 'Normal';
-      result_color = '#b1fdbb';
-    }
-    else if (total < 60) {
-      result = 'Mild';
-      result_color = 'yellow';
-    }
-    else if (total < 70) {
-      result = 'Moderate';
-      result_color = 'orange';
-    }
-    else {
-      result = 'Severe';
-      result_color = 'red';
-    }
-    console.log(total);
+    console.log(E, I, S, N, T, F, J, P);
     this.setData({
       result: total,
       result_text: result,
       result_color: result_color,
       hide_result: false,
+
     })
   },
 
